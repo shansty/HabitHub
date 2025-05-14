f5c6da1ec3bab21484158b16770f96928acb1d24
Anastasiya <shirochina16@gmail.com>
2025-05-14 12:55:21 +0200

    Add one more workflow for deploying to ECS and comment EC2,add task defeticion json for describing container setup

--- back-habit-hub/src/main.ts
+++ back-habit-hub/src/main.ts
@@ -23,6 +23,7 @@ async function start() {
     })
     await app.listen(PORT, '0.0.0.0', () => {
         console.log(`Server started on port ${PORT}`)
+        console.log("Log for checking deployment")
     })
 }
 start()
