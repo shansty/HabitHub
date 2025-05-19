f5f2c2dd1ddda96b95b797b69ebaddeaa6abf692
Anastasiya <shirochina16@gmail.com>
2025-05-19 12:21:09 +0200

    Add negative e2e test

--- front-habit-hub/tests/e2e.test.ts
+++ front-habit-hub/tests/e2e.test.ts
@@ -13,15 +13,6 @@ test.beforeEach(async ({ page }) => {
 
 test('Testing a form filled with correct data', async ({ page }) => {
 
-    // await page.goto('/');
-
-    // const heading = page.locator('h1');
-    // await expect(heading).toBeVisible();
-    // await expect(heading).toHaveText("Welcome to HabitHub – Your Personal Habit Tracker")
-    // const login_button = page.getByRole('button', { name: 'Log in' });
-    // await login_button.click();
-    // await expect(page).toHaveURL('/login');
-
     const username_field = page.getByPlaceholder("Enter your username")
     await username_field.fill("Anastasiya")
     const password_field = page.getByPlaceholder("Enter your password")
@@ -36,25 +27,14 @@ test('Testing a form filled with correct data', async ({ page }) => {
     await expect(add_habit_button).toBeVisible();
 });
 
-// test('Testing a form filled with incorrect data', async ({ page }) => {
-// 
-    // await page.goto('/');
+test('Testing a form filled with incorrect data', async ({ page }) => {
 
-    // const heading = page.locator('h1');
-    // await expect(heading).toBeVisible();
-    // await expect(heading).toHaveText("Welcome to HabitHub – Your Personal Habit Tracker")
-    // const login_button = page.getByRole('button', { name: 'Log in' });
-    // await login_button.click();
-    // await expect(page).toHaveURL('/login');
-
-    // const username_field = page.getByPlaceholder("Enter your username")
-    // await username_field.fill("Wrong")
-    // const password_field = page.getByPlaceholder("Enter your password")
-    // await password_field.fill('Data')
-    // const submit_button = page.getByRole('button', { name: 'Log in' });
-    // await submit_button.click();
-    // const error = page.getByTestId('custom-error');
-    // await expect(error).toHaveText('Invalid credentials. Please try again.', { timeout: 10000 });
-
-    // await expect(error).toHaveText('Invalid credentials. Please try again.');
-// });
+    const username_field = page.getByPlaceholder("Enter your username")
+    await username_field.fill("Wrong")
+    const password_field = page.getByPlaceholder("Enter your password")
+    await password_field.fill('Data')
+    const submit_button = page.getByRole('button', { name: 'Log in' });
+    await submit_button.click();
+    const error = page.getByTestId('custom-error');
+    await expect(error).toHaveText('Invalid credentials. Please try again.');
+});
